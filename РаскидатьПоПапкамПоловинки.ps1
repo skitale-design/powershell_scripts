cb9bd6888f765eec3df6b99b0660be26a6ba4bd4
skitale <skitale85@gmail.com>
2018-08-27 11:27:42 +0300

    initial

---
+++ РаскидатьПоПапкамПоловинки.ps1
@@ -0,0 +1,114 @@
+﻿function createFiles($number)
+{
+    1..$number | %{New-Item -Path "F:\Work\test" -Name $_'.txt' -ItemType File}
+}
+
+function createFolders($folder)
+{
+    1..2|%{
+        if(!(Test-Path "$folder\$_"))
+        {
+            ni -Path $folder -Name $_ -ItemType Directory        
+        }
+    }
+}
+
+function countFilesIn($folder)
+{
+    $cuantity = 0
+    foreach($_ in dir $folder)
+    {
+        if(!$_.PSisContainer)
+        {
+            $cuantity++;
+        }
+    }
+
+    $cuantity    
+}
+
+function InTwoFolders($folder)
+{
+    $filesInFolder = countFilesIn $folder
+    if((countFilesIn $folder) -ne 1)
+    {
+        createFolders $folder
+
+      <#  "folder = $folder"
+        "Файлов в директории $folder = $filesInFolder"
+        $folder1Exists =  (Test-Path $folder'\1')
+        $folder2Exists =  (Test-Path $folder'\2')
+
+        If(!$folder1Exists)
+        {
+            "Test-Path $folder'\1' --> $folder1Exists"
+            createFolders "$folder"
+        }
+
+        If(!$folder2Exists)
+        {
+            "Test-Path $folder'\2' --> $folder2Exists"            
+            createFolders "$folder"
+        }#>
+     }
+
+    "folder = $folder"
+    $i = 1
+    $half = (countFilesIn $folder)/2
+    "half=$half"
+    foreach($file in dir $folder)
+    {
+        if(!$file.PSisContainer)
+        {
+            $i
+            
+            if($i -le $half)
+            {
+                Copy-Item $file.FullName -Destination $folder'\1'   
+            }
+            else
+            {
+                Copy-Item $file.FullName -Destination $folder'\2'                   
+            }
+            $i++
+        }
+    }
+    $folder1 = $folder+'\1'
+    $folder2 = $folder+'\2'
+
+    $filesIn1 = countFilesIn($folder1)
+    $filesIn2 = countFilesIn($folder2)
+        
+    "В папке 1 файлов: $filesIn1"
+    "В папке 2 файлов: $filesIn2"
+
+    "Рекурсия"
+
+    if($filesIn1 -ne 1)
+    {
+        InTwoFolders $folder1
+    }
+
+    if($filesIn2 -ne 1)
+    {
+        InTwoFolders $folder2
+    }
+
+}
+
+function clearFolder($folder)
+{
+    dir $folder | %{Remove-Item $_.FullName -Force -Recurse}
+}
+
+function recurseFunc($folder)
+{
+    $i = countFilesIn $folder
+    do
+    {
+       InTwoFolders $folder
+       $folder
+    }
+    while ($i -ne 1)
+
+}
